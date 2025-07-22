eb3a1cb7035c59638f1be1b93fb68fc8a5837c1c
Andrés Marin <andresmarincelis@gmail.com>
2025-07-22 13:00:01 -0400

    Portfolio 3

--- app/(routes)/services/page.tsx
+++ app/(routes)/services/page.tsx
@@ -15,7 +15,7 @@ const Services = () => {
       <CoverParticles />
       <CircleImage />
       <AvatarServices />
-      <div className="grid items-center justify-center min-h-screen max-w-5xl gap-6 mx-auto md:grid-cols-2 px-4 pt-24">
+      <div className="grid items-center justify-center min-h-screen max-w-5xl gap-6 mx-auto md:grid-cols-2 px-4 pt-48">
         <div className="max-w-[450px] mt-10 md:mt-0 md:ml-5">
           <h1 className="text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-5">
             Mis
